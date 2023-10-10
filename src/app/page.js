6a0e925fa4ee4057480c950868aa76b9949ba012
laksmi santo <84493741+laksmisanto@users.noreply.github.com>
2023-10-11 00:52:42 +0600

    navbar design start

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,6 @@ export default function Home() {
     <>
       <Layout>
         <Banner />
-        <Logo />
       </Layout>
     </>
   );
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,9 +1,31 @@
 import React from "react";
+import Logo from "@/assets/svg/logo";
+import { MenuData } from "@/data/menuData";
+import Flex from "../flex/flex";
+import List from "../list/list";
+import ListItem from "../list/ListItem";
+import Container from "../container/container";
 
 const Navbar = () => {
   return (
     <>
-      <h2>this is navbar</h2>
+      <Container>
+        <Flex className="justify-between items-center py-4">
+          <Logo />
+          <List>
+            <Flex>
+              {MenuData.map((item, i) => (
+                <ListItem
+                  className="mx-4 cursor-pointer font-semibold transition-all ease-in-out duration-300 text-gray-500 hover:tracking-wider hover:text-gray-900 hover:font-semibold"
+                  key={i}
+                >
+                  {item.title}
+                </ListItem>
+              ))}
+            </Flex>
+          </List>
+        </Flex>
+      </Container>
     </>
   );
 };
